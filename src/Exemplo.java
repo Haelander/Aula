03b598c0fce98dc36d56bbc9f52f31b35d7c14a8
Felipe Alberto <Felipe.Alberto@uc.edu>
2021-06-17 21:54:26 -0300

    Adicionado helloManyTimes.

--- src/Exemplo.java
+++ src/Exemplo.java
@@ -1,5 +1,15 @@
 public class Exemplo {
-    public static void main(String[] args) {
+    private static void sayHelloManyTimes(int times) {
+        for (int i = 0; i < times; i++) {
+            sayHello();
+        }
+    }
+
+    private static void sayHello() {
         System.out.println("Olá mundo!");
+    }
+
+    public static void main(String[] args) {
+        sayHelloManyTimes(5);
     }
 }
